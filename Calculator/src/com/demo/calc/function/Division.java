f2190b96d5f753900509576bc104277c2b8f1f14
unknown <maheshsaggam9@gmail.com>
2024-11-04 23:07:17 +0530

    Division feature

---
+++ Calculator/src/com/demo/calc/function/Division.java
@@ -0,0 +1,13 @@
+package com.demo.calc.function;
+
+public class Division {
+
+	public Division() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int division(int a, int b) {
+		return a/b;
+	}
+
+}
